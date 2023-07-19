c043dd6961c2653d293f8bf5f4a82fa0483855a4
Karim El Refai <100537642+urfavcrow23@users.noreply.github.com>
2023-07-19 04:46:02 +0300

    Update B- Divisibility Problem.cpp

--- Sheet 5/B- Divisibility Problem.cpp
+++ Sheet 5/B- Divisibility Problem.cpp
@@ -25,3 +25,27 @@ while(t--){
 
 	return 0;
 }
+
+
+// Solution 2 --> Upsolving Session
+# include <iostream>
+using namespace std;
+
+int main(){
+	//? Brute Force 
+	//? 10 4, 11 4, 12 4
+	int t;
+	cin >> t;
+	for(int ti = 0; ti < t; ti++){
+		int a, b;
+		cin >> a >> b;
+		if (a % b == 0){
+			cout << 0 << endl;
+		}
+		else{
+			int ans = b - a % b; // O(1)
+			cout << ans << endl;
+		}
+	}
+	return 0;
+}
