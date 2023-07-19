5267f8cfcb0dc267f2242090a6e3c22bc2e6132e
Karim El Refai <100537642+urfavcrow23@users.noreply.github.com>
2023-07-19 04:45:11 +0300

    Update D- Olesya and Rodion.cpp

--- Sheet 5/D- Olesya and Rodion.cpp
+++ Sheet 5/D- Olesya and Rodion.cpp
@@ -23,3 +23,37 @@ int main()
       }     
       return 0;
 }
+
+// Solution 2 --> Upsolving Session 
+# include <iostream>
+using namespace std;
+
+int main(){
+      int n, t;
+      cin >> n >> t;
+
+      //  t = 10
+      // n = 3, t = 7 700
+      // n = 4, t = 10 1000
+      // Wrong Answer?
+      // Edge Case n = 1, t = 10
+
+      if(t == 10){
+            if(n == 1){
+                  cout << -1;
+            }
+            else{
+                  cout << t;
+                  for(int i = 0; i < n - 2; i++){
+                        cout << 0;
+                  }
+            }
+      }
+      else{
+            for(int i = 0; i < n; i++){
+                  cout << t;
+            }
+      }
+      cout << '\n';
+      return 0;
+}
